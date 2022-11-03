0a6325ff296da9815b9e11fd9b4a240853de8ecc
Josue Palomo <josuepalomocs@gmail.com>
2022-11-03 15:07:03 -0500

    Add initial project files

---
+++ src/test/java/com/josuepalomocs/perfin/PerfinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josuepalomocs.perfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
