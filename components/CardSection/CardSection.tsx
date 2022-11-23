a975d432075682097231864de1d1ef54d4ee6265
Josue Palomo <josuepalomocs@gmail.com>
2022-11-23 10:37:04 -0600

    Edit card section styling

--- components/CardSection/CardSection.tsx
+++ components/CardSection/CardSection.tsx
@@ -27,83 +27,85 @@ const CardSection = ({} : CardSectionProps) => {
 
   return (
     <Container className={styles.container}>
-      <Box className={styles.actions}>
-        <Fab className={`${styles.fab} ${styles.copyCardNumber}`} onClick={() => {activeCard && copyCardNumberToClipboard(activeCard.number)}}>
-          <ClipboardIcon className={`${styles.icon} ${styles.clipboardIcon}`} />
-        </Fab>
-        <Fab className={`${styles.fab} ${styles.revealCard}`} onClick={() => {setCardRevealed(!cardRevealed)}}>
+      <Box className={styles.grid}>
+        <Box className={styles.actions}>
+          <Fab className={`${styles.fab} ${styles.copyCardNumber}`} onClick={() => {activeCard && copyCardNumberToClipboard(activeCard.number)}}>
+            <ClipboardIcon className={`${styles.icon} ${styles.clipboardIcon}`} />
+          </Fab>
+          <Fab className={`${styles.fab} ${styles.revealCard}`} onClick={() => {setCardRevealed(!cardRevealed)}}>
+            {
+              cardRevealed ?
+                <EyeSlashIcon className={`${styles.icon} ${styles.eyeSlashIcon}`} /> :
+                <EyeIcon className={`${styles.icon} ${styles.eyeIcon}`} />
+            }
+          </Fab>
+          <Fab className={`${styles.fab} ${styles.deleteCard}`} onClick={() => {activeCard && setDeleteCardDialogOpen(true)}}>
+            <TrashIcon className={`${styles.icon} ${styles.trashIcon}`} />
+          </Fab>
+          <Fab className={`${styles.fab} ${styles.editCard}`} onClick={() => {activeCard && setEditCardDialogOpen(true)}}>
+            <PencilSquareIcon className={`${styles.icon} ${styles.pencilSquareIcon}`} />
+          </Fab>
+          <Fab className={`${styles.fab} ${styles.createCard}`} onClick={() => {setCreateCardDialogOpen(true)}}>
+            <PlusIcon className={`${styles.icon} ${styles.plusIcon}`} />
+          </Fab>
+        </Box>
+        <Button 
+          className={`${styles.toPreviousCard} ${cardSelection <= 0 && styles.firstCard}`} 
+          variant='contained' 
+          onClick={() => {cardSelection > 0 && setCardSelection(cardSelection - 1)}}>
+          <ChevronLeftIcon className={styles.chevronLeftIcon} />
+        </Button>
+        <Box className={styles.card}>
           {
-            cardRevealed ?
-              <EyeSlashIcon className={`${styles.icon} ${styles.eyeSlashIcon}`} /> :
-              <EyeIcon className={`${styles.icon} ${styles.eyeIcon}`} />
+            activeCard ?
+              <Card
+                brand={activeCard.brand}
+                nickname={activeCard.nickname}
+                number={cardRevealed ? 
+                  formattedCardNumber! :
+                  `${activeCard.brand === 'american-express' ? 
+                    `•••• •••••• ${formattedCardNumber!.slice(-5)}` : 
+                      `•••• •••• •••• ${formattedCardNumber!.slice(-5)}`}`}
+                expirationDate={cardRevealed ? activeCard.expirationDate : '••/••'}
+                csv={cardRevealed ? activeCard.csv : '•••'}
+              /> :
+              <Typography className={styles.noCardsText}>You don't have any saved cards</Typography>
           }
-        </Fab>
-        <Fab className={`${styles.fab} ${styles.deleteCard}`} onClick={() => {activeCard && setDeleteCardDialogOpen(true)}}>
-          <TrashIcon className={`${styles.icon} ${styles.trashIcon}`} />
-        </Fab>
-        <Fab className={`${styles.fab} ${styles.editCard}`} onClick={() => {activeCard && setEditCardDialogOpen(true)}}>
-          <PencilSquareIcon className={`${styles.icon} ${styles.pencilSquareIcon}`} />
-        </Fab>
-        <Fab className={`${styles.fab} ${styles.createCard}`} onClick={() => {setCreateCardDialogOpen(true)}}>
-          <PlusIcon className={`${styles.icon} ${styles.plusIcon}`} />
-        </Fab>
-      </Box>
-      <Button 
-        className={`${styles.toPreviousCard} ${cardSelection <= 0 && styles.firstCard}`} 
-        variant='contained' 
-        onClick={() => {cardSelection > 0 && setCardSelection(cardSelection - 1)}}>
-        <ChevronLeftIcon className={styles.chevronLeftIcon} />
-      </Button>
-      <Box className={styles.card}>
+        </Box>
+        <Button 
+          className={`${styles.toNextCard} ${cards && cardSelection === cards.length - 1 && styles.lastCard}`} 
+          variant='contained' 
+          onClick={() => {cards && cardSelection < cards.length - 1 && setCardSelection(cardSelection + 1)}}>
+          <ChevronRightIcon className={styles.chevronRightIcon} />
+        </Button>
+        <CreateCardDialog
+          dialogOpen={createCardDialogOpen}
+          setDialogOpen={setCreateCardDialogOpen}
+          cards={cards}
+          setCards={setCards}
+          cardSelection={cardSelection}
+          setCardSelection={setCardSelection} />
         {
-          activeCard ?
-            <Card
-              brand={activeCard.brand}
-              nickname={activeCard.nickname}
-              number={cardRevealed ? 
-                formattedCardNumber! :
-                `${activeCard.brand === 'american-express' ? 
-                  `•••• •••••• ${formattedCardNumber!.slice(-5)}` : 
-                    `•••• •••• •••• ${formattedCardNumber!.slice(-5)}`}`}
-              expirationDate={cardRevealed ? activeCard.expirationDate : '••/••'}
-              csv={cardRevealed ? activeCard.csv : '•••'}
-            /> :
-            <Typography className={styles.noCardsText}>You don't have any saved cards</Typography>
+          cards &&
+          <DeleteCardDialog
+            dialogOpen={deleteCardDialogOpen}
+            setDialogOpen={setDeleteCardDialogOpen}
+            cards={cards!}
+            setCards={setCards}
+            cardSelection={cardSelection}
+            setCardSelection={setCardSelection} />
+        }
+        {
+          cards &&
+          <EditCardDialog
+            dialogOpen={editCardDialogOpen} 
+            setDialogOpen={setEditCardDialogOpen} 
+            cards={cards!} 
+            setCards={setCards}
+            cardSelection={cardSelection}
+            setCardSelection={setCardSelection} />
         }
       </Box>
-      <Button 
-        className={`${styles.toNextCard} ${cards && cardSelection === cards.length - 1 && styles.lastCard}`} 
-        variant='contained' 
-        onClick={() => {cards && cardSelection < cards.length - 1 && setCardSelection(cardSelection + 1)}}>
-        <ChevronRightIcon className={styles.chevronRightIcon} />
-      </Button>
-      <CreateCardDialog
-        dialogOpen={createCardDialogOpen}
-        setDialogOpen={setCreateCardDialogOpen}
-        cards={cards}
-        setCards={setCards}
-        cardSelection={cardSelection}
-        setCardSelection={setCardSelection} />
-      {
-        cards &&
-        <DeleteCardDialog
-          dialogOpen={deleteCardDialogOpen}
-          setDialogOpen={setDeleteCardDialogOpen}
-          cards={cards!}
-          setCards={setCards}
-          cardSelection={cardSelection}
-          setCardSelection={setCardSelection} />
-      }
-      {
-        cards &&
-        <EditCardDialog
-          dialogOpen={editCardDialogOpen} 
-          setDialogOpen={setEditCardDialogOpen} 
-          cards={cards!} 
-          setCards={setCards}
-          cardSelection={cardSelection}
-          setCardSelection={setCardSelection} />
-      }
     </Container>
   );
 };
